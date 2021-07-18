456911b0bb51c587579c682cbd7f0d0e00fa4bcb
Pridgey <musicinsilence13@gmail.com>
2021-07-17 23:22:51 -0600

    main/ console log the socket

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const options = {
 const io = require("socket.io")(process.env.PORT || 80, options);
 
 io.on("connection", (socket) => {
+  console.log("========");
+  console.log("socket:", socket);
+  console.log("========");
+
   socket.on("message", (message) => {
     console.log("Message:", message);
     socket.send(message);
